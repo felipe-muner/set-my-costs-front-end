15588c6d670d0f413395b4b2d074adacdd652980
felipemuner <felipe.muner@gmail.com>
2020-02-04 12:42:31 +0700

    showing the currencies

--- src/components/ExchangeRate.vue
+++ src/components/ExchangeRate.vue
@@ -1,9 +1,31 @@
 <template>
   <div>
-    <span>Rates Today</span>
-    <ul>
-      <li v-for="r in getRates" v-bind:key="r.CurrencyID">{{ r }}</li>
-    </ul>
+    <v-alert
+      type="prominent"
+      border="left"
+      color="green"
+      icon="mdi-account"
+      transition="slide-y-reverse-transition"
+    >Rates day : {{getRates}}</v-alert>
+    <br />
+    <v-container>
+      <v-row no-gutters>
+        <v-col></v-col>
+        <v-col>
+          <v-card outlined tile class="ma-3">
+            <v-data-table
+              dense
+              dark
+              :headers="headers"
+              :items="getRates"
+              hide-default-footer
+              class="elevation-10"
+            ></v-data-table>
+          </v-card>
+        </v-col>
+        <v-col></v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script>
@@ -11,12 +33,22 @@ import { mapGetters } from "vuex";
 import { mapActions } from "vuex";
 
 export default {
+  data: () => ({
+    headers: [
+      {
+        text: "Curreny Name",
+        align: "left",
+        sortable: false,
+        value: "CurrencyName"
+      },
+      { text: "Rate", value: "Rate" }
+    ]
+  }),
   computed: {
     ...mapGetters(["getRates"])
   },
-  data: () => ({}),
   methods: {
-    ...mapActions(["loadRates", "loadUsers"])
+    ...mapActions(["loadRates"])
   },
   created() {
     this.loadRates();
