debc07bf5453b01ceed260956f0388b62bf14aad
felipemuner <felipe.muner@gmail.com>
2020-02-04 11:23:07 +0700

    setting vuex

--- src/components/ExchangeRate.vue
+++ src/components/ExchangeRate.vue
@@ -1,28 +1,51 @@
 <template>
-  <div>sou o component exchange rate</div>
+  <div>
+    <span>Rates Today</span>
+    <ul>
+      <li v-for="r in exchangeRateQueue" v-bind:key="r.CurrencyID">{{ r }}</li>
+    </ul>
+    <hr />
+    <hr />
+    <div>{{products}}</div>
+    <hr />
+    <div>{{saleProducts}}</div>
+    <div>usuario</div>
+    <v-btn v-on:click="loadUsers()">Get Users</v-btn>
+    <div>{{getUsers}}</div>
+    <hr />
+    <hr />
+    <div>rates</div>
+    <div>{{getRates}}</div>
+  </div>
 </template>
 <script>
+import { mapState } from "vuex";
+import { mapGetters } from "vuex";
+import { mapActions } from "vuex";
+
 export default {
+  computed: {
+    ...mapState(["products"]),
+    ...mapGetters(["saleProducts", "getUsers", "getRates"])
+  },
   data: () => ({
     exchangeRateQueue: []
   }),
   methods: {
-    getExchangeRate() {
-      return this.$http.get(this.$API + "/exchange-rate");
-    }
+    ...mapActions(["loadRates", "loadUsers"])
+    // getExchangeRate() {
+    //   return this.$http.get(this.$API + "/exchange-rate");
+    // }
   },
-
   created() {
-    this.getExchangeRate()
-      .then(er => {
-        console.log("sem prob");
-
-        this.exchangeRateQueue = er.data.exchangeRateQueue;
-      })
-      .catch(error => {
-        console.log("entrei erro");
-        console.log(error);
-      });
+    setTimeout(() => {
+      this.loadUsers();
+      this.loadRates();
+      // this.$store.dispatch("reducePrice", amount);
+      // this.getExchangeRate()
+      //   .then(er => (this.exchangeRateQueue = er.data.rates))
+      //   .catch(error => console.log(error));
+    }, 2000);
   }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,78 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    users: [
+      {
+        id: 1,
+        first_name: "Donalt",
+        last_name: "Ralling",
+        email: "dralling0@paginegialle.it",
+        gender: "Male",
+        ip_address: "173.68.122.24"
+      },
+      {
+        id: 2,
+        first_name: "Maren",
+        last_name: "Quan",
+        email: "mquan1@unc.edu",
+        gender: "Female",
+        ip_address: "85.49.14.181"
+      },
+      {
+        id: 3,
+        first_name: "Janos",
+        last_name: "Budnk",
+        email: "jbudnk2@example.com",
+        gender: "Male",
+        ip_address: "210.14.247.164"
+      }
+    ],
+    rates: [2, 4, 9],
+    products: [
+      { id: 1, name: "Banana Skin", price: 20 },
+      { id: 2, name: "Shiny Star", price: 40 },
+      { id: 3, name: "Green Shells", price: 60 },
+      { id: 4, name: "Red Shells", price: 80 }
+    ]
+  },
+  getters: {
+    getRates: state => state.rates,
+    saleProducts: state => {
+      let valeProducts = state.products.map(product => {
+        return {
+          name: "**" + product.name + "**",
+          price: product.price / 2
+        };
+      });
+      return valeProducts;
+    },
+    getUsers: state => state.users
   },
   mutations: {
+    SAVE_USERS(state, users) {
+      state.users = users;
+    },
+    SAVE_RATES(state, rates) {
+      state.rates = rates;
+    }
   },
   actions: {
+    async loadUsers({ commit }) {
+      const response = await fetch("https://api.myjson.com/bins/l4tgy");
+      const clients = await response.json();
+      commit("SAVE_USERS", clients);
+    },
+    async loadRates({ commit }) {
+      const response = await this._vm.$http.get(
+        this._vm.$API + "/exchange-rate"
+      );
+      const rates = await response.data;
+      commit("SAVE_RATES", rates);
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
