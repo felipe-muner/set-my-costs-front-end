d8be5dfbb61d51f001fa0e4abdd6bad782e4ae89
felipemuner <felipe.muner@gmail.com>
2020-02-10 11:59:50 +0700

    changing the class constructor and setting separated functions

--- api/costCenter.js
+++ api/costCenter.js
@@ -2,11 +2,20 @@ import axios from "axios";
 
 class CostCenter {
   constructor(server) {
-    this.loadCostCenter = async () => axios.get(server + "/cost-center");
-    this.saveCostCenter = async state =>
-      axios.post(server + "/cost-center/save", {
-        Name: state.nameCostCenter
-      });
+    this.server = server;
+    //   this.loadCostCenter = async () => axios.get(server + "/cost-center");
+    //   this.saveCostCenter = async state =>
+    //     axios.post(server + "/cost-center/save", {
+    //       Name: state.nameCostCenter
+    //     });
+  }
+  async loadCostCenter() {
+    return axios.get(this.server + "/cost-center");
+  }
+  async saveCostCenter(state) {
+    return axios.post(this.server + "/cost-center/save", {
+      Name: state.nameCostCenter
+    });
   }
 }
 
